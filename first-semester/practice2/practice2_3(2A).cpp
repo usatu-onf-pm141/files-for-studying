4f047aadb5fda21a9a8f8ba65aaaf71d6ac8135d
SashOKNovikOFF <SashOKNovikOFF@yandex.ru>
2013-10-24 19:55:49 +0600

    Update The Second Practice
    Added another task by Artur Ganeev.

---
+++ first-semester/practice2/practice2_3(2A).cpp
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void main()
+{ int a;
+cout « "input finish number of sqr ";
+cin » a;
+int i;
+for (i=0; i<=a; i++)
+cout « 2 « " " « pow ((double)2,i) « '\n'; 
+_getch();
+}
